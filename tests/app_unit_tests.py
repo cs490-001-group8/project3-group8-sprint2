e5e3ac8194b663af51d41be1d77750a25fcaf485
EC2 Default User <ec2-user@ip-172-31-2-238.ec2.internal>
2020-10-31 18:21:39 +0000

    Improve unit tests on app.py

--- tests/app_unit_tests.py
+++ tests/app_unit_tests.py
@@ -3,9 +3,12 @@
     This file does all non-mocked unit tests
 """
 import unittest
+import unittest.mock as mock
 import sys
 from os.path import dirname, join
 
+sys.path.append(join(dirname(__file__), "../"))
+import app
 
 # pylint: disable=R0902
 class AppTestCases(unittest.TestCase):
@@ -17,11 +20,16 @@ class AppTestCases(unittest.TestCase):
         """Set up test cases"""
         self.success_test_params = [
         ]
+        
+    def mocked_flask_render(self, url):
+        """Mock Flask render"""
+        if not isinstance(url, str):
+            raise ValueError("URL not string")
 
     def test_app_runs_success(self):
         """Test successful test cases"""
-        sys.path.append(join(dirname(__file__), "../"))
-        import app
+        with mock.patch("flask.render_template", self.mocked_flask_render):
+            app.hello()
 
 
 if __name__ == "__main__":
