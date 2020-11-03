26030dedbb4975f3022d1dfbc2a6d9dd7b6ce714
Ameer Hassan <ec2-user@ip-172-31-64-222.ec2.internal>
2020-11-03 20:29:50 +0000

    server-side oauth completed

--- scripts/Body.jsx
+++ scripts/Body.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import OAuthButton from './OAuthButton'
 
 export default function Body() {
   return (
     <div className="Body">
-      <OAuthButton />
       This is a body.
     </div>
   );
--- scripts/OAuthButton.jsx
+++ scripts/OAuthButton.jsx
@@ -3,25 +3,27 @@ import { GoogleLogin } from 'react-google-login';
 import { Socket } from './Socket';
 
 export default function OAuthButton() {
-    let clientID = '881732433179-i733jq65m0j09pio425p40hosd8r2klf.apps.googleusercontent.com';
-    
-    const responseGoogle = (response) => {
-        Socket.emit('user login', {
-            'id': response.profileObj.googleId,
-            'name': response.profileObj.name,
-            'image': response.profileObj.imageUrl,
-            'email': response.profileObj.email
-        });
+  let clientID = '881732433179-i733jq65m0j09pio425p40hosd8r2klf.apps.googleusercontent.com';
+  
+  const responseGoogle = (response) => {
+    let userObject = {
+      'id': response.profileObj.googleId,
+      'name': response.profileObj.name,
+      'image': response.profileObj.imageUrl,
+      'email': response.profileObj.email
     }
- 
-    return (
-        <GoogleLogin
-            clientId={clientID}
-            buttonText="Login"
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-            cookiePolicy={'single_host_origin'}
-            id="googleButton"
-        />
-    )
+    
+    Socket.emit('user login', userObject);
+  }
+
+  return (
+    <GoogleLogin
+      clientId={clientID}
+      buttonText="Login"
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      cookiePolicy={'single_host_origin'}
+      id="googleButton"
+    />
+  )
 }
