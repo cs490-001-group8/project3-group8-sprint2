b1da5a694d115566bd23d37f79b8ad0668cec6c4
Ameer Hassan <ec2-user@ip-172-31-64-222.ec2.internal>
2020-11-03 20:46:29 +0000

    added unit testing for oauth login

--- tests/app_unit_tests.py
+++ tests/app_unit_tests.py
@@ -30,7 +30,21 @@ class AppTestCases(unittest.TestCase):
         """Test successful test cases"""
         with mock.patch("flask.render_template", self.mocked_flask_render):
             app.hello()
-
+    
+    def mocked_user_object(self, user_object):
+        if not isinstance(user_object, dict):
+            raise ValueError("user_object is not a dictionary")
+    
+    def test_on_user_login(self):
+        test_user = {
+            "id": "xxxxxxx",
+            "name": "Ameer",
+            "image": "https://xxxxx",
+            "email": "xxx@xxxx.com"
+        }
+        with mock.patch("app.on_user_login", self.mocked_user_object):
+            app.on_user_login(test_user)
+            self.assertEqual(4, len(test_user))
 
 if __name__ == "__main__":
     unittest.main()
