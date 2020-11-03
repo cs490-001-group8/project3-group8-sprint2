53ca8ad0d7b6267ce0a0ad1ac9e0c22ef30e76aa
Akash Patel <ec2-user@ip-172-31-69-21.ec2.internal>
2020-11-03 22:44:48 +0000

    Basic Google Traffic Map with main focus on New Jersey state has been display on the screen and the style in place for major roads that pop out as green without any overlay if there is no traffic other different overlays with different color such as red,yellow, etc.

---
+++ scripts/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import GoogleTrafficView from './GoogleTrafficView';
+
+export default function App() {
+  return (
+    <GoogleTrafficView />
+  );
+}
--- scripts/Main.jsx
+++ scripts/Main.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-  <React.StrictMode>
-    Hello, World
-  </React.StrictMode>,
+  <App />,
   rootElement,
 );
