12237281c18e4e5ebdc39e644edc50bb22aa5d07
Joseph Fiore <ec2-user@ip-172-31-2-238.ec2.internal>
2020-11-03 18:11:35 +0000

    Improve linting for app unit tests

--- tests/app_unit_tests.py
+++ tests/app_unit_tests.py
@@ -7,10 +7,12 @@ import unittest.mock as mock
 import sys
 from os.path import dirname, join
 
+# pylint: disable=C0413
 sys.path.append(join(dirname(__file__), "../"))
 import app
 
 # pylint: disable=R0902
+# pylint: disable=R0201
 class AppTestCases(unittest.TestCase):
     """Make all the test cases"""
 
@@ -18,23 +20,23 @@ class AppTestCases(unittest.TestCase):
 
     def setUp(self):
         """Set up test cases"""
-        self.success_test_params = [
-        ]
-        
+        self.success_test_params = []
+
     def mocked_flask_render(self, url):
         """Mock Flask render"""
         if not isinstance(url, str):
             raise ValueError("URL not string")
 
     def mock_session_commit(self):
+        """Mock Session commit"""
         return
-    
+
     def mock_session_add_comment(self, comment):
+        """Mock Session add for comments"""
         if not isinstance(comment.tab, str):
             raise ValueError("Tab not string")
         if not isinstance(comment.text, str):
             raise ValueError("Text not string")
-        return
 
     def test_app_runs_success(self):
         """Test successful test cases"""
@@ -42,14 +44,23 @@ class AppTestCases(unittest.TestCase):
             app.hello()
 
     def test_app_new_comment_success(self):
-        with mock.patch("sqlalchemy.orm.session.Session.commit", self.mock_session_commit
-        ), mock.patch("sqlalchemy.orm.session.Session.add", self.mock_session_add_comment):
+        """Test successful new comments"""
+        with mock.patch(
+                "sqlalchemy.orm.session.Session.commit", self.mock_session_commit
+        ), mock.patch(
+            "sqlalchemy.orm.session.Session.add", self.mock_session_add_comment
+        ):
             app.on_new_comment({"text": "Hello, I'm Joe", "tab": "Home"})
 
     def test_app_new_comment_failure(self):
-        with mock.patch("sqlalchemy.orm.session.Session.commit", self.mock_session_commit
-        ), mock.patch("sqlalchemy.orm.session.Session.add", self.mock_session_add_comment):
+        """Test failed new comments"""
+        with mock.patch(
+                "sqlalchemy.orm.session.Session.commit", self.mock_session_commit
+        ), mock.patch(
+            "sqlalchemy.orm.session.Session.add", self.mock_session_add_comment
+        ):
             app.on_new_comment({"text": "Hello, I'm Joe"})
+
 
 if __name__ == "__main__":
     unittest.main()
