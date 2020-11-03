24076a3390840152bb7bfa5aa10fe5119aac3223
Ameer Hassan <ec2-user@ip-172-31-64-222.ec2.internal>
2020-11-03 19:47:49 +0000

    Finished client-side oauth

--- scripts/Body.jsx
+++ scripts/Body.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import OAuthButton from './OAuthButton'
 
 export default function Body() {
   return (
     <div className="Body">
+      <OAuthButton />
       This is a body.
     </div>
   );
