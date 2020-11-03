e5f928a4686b1e8887281c2b5b3387b6a3d641a4
Joseph Fiore <ec2-user@ip-172-31-2-238.ec2.internal>
2020-11-03 19:45:17 +0000

    Improve formatting on app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-'''
+"""
     app.py
     This file launches the flask server for the app
-'''
+"""
 import os
 import flask
 import flask_socketio
@@ -16,36 +16,43 @@ APP = flask.Flask(__name__)
 SOCKETIO = flask_socketio.SocketIO(APP)
 SOCKETIO.init_app(APP, cors_allowed_origins="*")
 
-engine = sqlalchemy.create_engine(os.environ['DATABASE_URL'])
-BASE.metadata.create_all(engine, checkfirst=True)
+ENGINE = sqlalchemy.create_engine(os.environ["DATABASE_URL"])
+BASE.metadata.create_all(ENGINE, checkfirst=True)
 
-Session = sqlalchemy.orm.sessionmaker(bind=engine)
-session = Session()
+SESSION_MAKER = sqlalchemy.orm.sessionmaker(bind=ENGINE)
+SESSION = SESSION_MAKER()
 
-@APP.route('/')
+
+@APP.route("/")
 def hello():
-    '''When someone opens the app, send them the page'''
-    return flask.render_template('index.html')
+    """When someone opens the app, send them the page"""
+    return flask.render_template("index.html")
 
-@SOCKETIO.on('new comment')
+
+@SOCKETIO.on("new comment")
 def on_new_comment(data):
-    '''Process a new comment'''
+    """Process a new comment"""
     try:
         new_text = data["text"]
         which_tab = data["tab"]
-        session.add(tables.Comment(new_text, which_tab))
-        session.commit()
-        all_comments_tab = [{"text": comment.text}
-            for comment in session.query(tables.Comment).filter(tables.Comment.tab == which_tab).all()]
+        SESSION.add(tables.Comment(new_text, which_tab))
+        SESSION.commit()
+        all_comments_tab = [
+            {"text": comment.text}
+            for comment in SESSION.query(tables.Comment)
+            .filter(tables.Comment.tab == which_tab)
+            .all()
+        ]
         all_comments_tab.reverse()
         SOCKETIO.emit("new comment", {"comments": all_comments_tab})
     except KeyError:
         return
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     SOCKETIO.run(
         APP,
-        host=os.getenv('IP', '0.0.0.0'),
-        port=int(os.getenv('PORT', '8080')),
-        debug=True
+        host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", "8080")),
+        debug=True,
     )
