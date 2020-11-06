5e04ea6e6e249dad8bb019a8c93b61db2e02cdb0
Ameer Hassan <ec2-user@ip-172-31-64-222.ec2.internal>
2020-11-06 04:56:59 +0000

    linting of jsx

--- scripts/WeatherHourly.jsx
+++ scripts/WeatherHourly.jsx
@@ -1,27 +1,49 @@
-import React, { useState } from 'react';
-import WidgetTitle from './WidgetTitle';
-import { Socket } from './Socket';
+import React from 'react';
 
-function convertTime(time){
-    let newTime = ""
-    if (time > 12){
-        newTime = time - 12 + ":00 PM"
-    } else if (time === 0){
-        newTime = "12:00 AM"
-    } else {
-        newTime = time + ":00 AM"
-    }
-    
-    return newTime
+function convertTime(time) {
+  let newTime = '';
+
+  if (time > 12) {
+    newTime = time - 12 + ':00 PM'; // eslint-disable-line prefer-template
+  } else if (time === 0) {
+    newTime = '12:00 AM'; // eslint-disable-line prefer-template
+  } else {
+    newTime = time + ':00 AM'; // eslint-disable-line prefer-template
+  }
+
+  return newTime;
 }
 
 export default function WeatherHourly(props) {
+  const {
+    time,
+    icon,
+    description,
+    temp,
+  } = props;
+
   return (
     <div className="hourlyWeather">
-        <h2>{convertTime(props.time)}</h2>
-        <img src={props.icon} alt={props.description} />
-        <h4> {props.description} </h4>
-        <h3> {props.temp} </h3>
+      <h2>
+        {convertTime(time)}
+      </h2>
+      <img
+        src={icon}
+        alt={description}
+      />
+      <h4>
+        {description}
+      </h4>
+      <h3>
+        {temp}
+      </h3>
     </div>
   );
 }
+
+WeatherHourly.propTypes = {
+  time: WeatherHourly.string.isRequired,
+  icon: WeatherHourly.string.isRequired,
+  description: WeatherHourly.string.isRequired,
+  temp: WeatherHourly.string.isRequired,
+};
--- scripts/WeatherModule.jsx
+++ scripts/WeatherModule.jsx
@@ -1,53 +1,54 @@
 import React, { useState } from 'react';
 import WidgetTitle from './WidgetTitle';
-import WeatherHourly from './WeatherHourly.jsx'
+import WeatherHourly from './WeatherHourly';
 import { Socket } from './Socket';
 
-
 function handleSubmit(event) {
-    let currCity = document.getElementById("inputCity");
-    let currCityValue = currCity.value;
-    Socket.emit('weather request', {
-        'city_name': currCityValue,
-    });
-    
-    currCity.value = '';
-    event.preventDefault();
+  const currCity = document.getElementById('inputCity');
+  const currCityValue = currCity.value;
+  Socket.emit('weather request', {
+    city_name: currCityValue,
+  });
+
+  currCity.value = '';
+  event.preventDefault();
 }
 
 export default function WeatherModule() {
   const [currWeather, setCurrWeather] = useState(() => []);
-  const [currCity, setCurrCity] = useState(() => "Hourly")
-  
+  const [currCity, setCurrCity] = useState(() => 'Hourly');
+
   function recieveWeather() {
     React.useEffect(() => {
       Socket.on('send weather', (data) => {
-        setCurrCity(data.city_name)
-        setCurrWeather([])
-        for (let i=0; i<data.hourly.length; i++){
-          let iterWeather = <WeatherHourly
-                              key={i}
-                              time={data.hourly[i].time}
-                              icon={data.hourly[i].icon}
-                              description={data.hourly[i].description}
-                              temp={Math.round(data.hourly[i].temp) + " °F"}
-                            />;
-          setCurrWeather(currWeather => [...currWeather, iterWeather])
+        setCurrCity(data.city_name);
+        setCurrWeather([]);
+        for (let i = 0; i < data.hourly.length; i += 1) {
+          const iterWeather = (
+            <WeatherHourly
+              key={i}
+              time={data.hourly[i].time}
+              icon={data.hourly[i].icon}
+              description={data.hourly[i].description}
+              temp={`${Math.round(data.hourly[i].temp)} °F`}
+            />
+          );
+          // eslint-disable-next-line no-shadow
+          setCurrWeather((currWeather) => [...currWeather, iterWeather]);
         }
-      })
+      });
 
       return () => {
-        Socket.off("user login");
+        Socket.off('user login');
       };
-      
     });
   }
-  
+
   recieveWeather();
-  
+
   return (
     <div className="widget weather_widget">
-      <WidgetTitle title={currCity + " Forecast"}/>
+      <WidgetTitle title={`${currCity} Forecast`} />
       <form onSubmit={handleSubmit}>
         <input id="inputCity" placeholder="Input your city name here..." type="text" />
       </form>
