9c41d571118ea320aa87090a4f0677f254f1ab6d
Bishoy Kamel <ec2-user@ip-172-31-36-77.ec2.internal>
2020-11-06 20:19:46 +0000

    fixed linting of news list

--- scripts/NewsList.jsx
+++ scripts/NewsList.jsx
@@ -1,43 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import { Item, Placeholder, Image } from 'semantic-ui-react'
-import WidgetTitle from './WidgetTitle'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import { Item, Placeholder, Image } from 'semantic-ui-react';
+import axios from 'axios';
+import WidgetTitle from './WidgetTitle';
 
-<<<<<<< HEAD
-const apiURL = "https://gnews.io/api/v4/search?q=new jersey&token="
-=======
-const apiURL = "https://newsapi.org/v2/everything?q=new-jersey&apiKey="
->>>>>>> 59a74e2a5a66c7176f65c48a1ffa206f6f158c2c
+const apiURL = 'https://gnews.io/api/v4/search?q=new jersey&token=';
 
 export default function NewsList() {
-    const [news, setNews] = useState(() => [{}, {}, {}, {}, {},])
-    const [loading, setLoading] = useState(() => true)
+    const [news, setNews] = useState(() => [
+        {
+            author: 'A',
+            publishedAt: '2020-11-04',
+        },
+        {
+            author: 'B',
+            publishedAt: '2020-11-04',
+        },
+        {
+            author: 'C',
+            publishedAt: '2020-11-04',
+        },
+        {
+            author: 'D',
+            publishedAt: '2020-11-04',
+        },
+        {
+            author: 'E',
+            publishedAt: '2020-11-04',
+        },
+    ]);
+    const [loading, setLoading] = useState(() => true);
 
     useEffect(() => {
         axios.get(`${apiURL}${process.env.REACT_APP_NEWS_API_KEY}`).then((response) => {
-            setNews(response.data.articles.slice(0, 5))
-            setLoading(false)
-        })
-    })
+            setNews(response.data.articles.slice(0, 5));
+            setLoading(false);
+        });
+    });
 
     return (
         <div className="widget">
             <WidgetTitle title="News" />
             <Item.Group className="news-group">
-                {news.map((item, index) => (
-                    <Item className="news-wrapper" key={index}>
+                {news.map((item) => (
+                    <Item className="news-wrapper" key={item.author + item.publishedAt}>
                         <Item.Image size="tiny" className="news-image">
                             {loading ? (
                                 <Placeholder className="news-image">
                                     <Placeholder.Image />
                                 </Placeholder>
                             ) : (
-<<<<<<< HEAD
-                                    <Image src={item.image} className="news-image" />
-=======
-                                    <Image src={item.urlToImage} className="news-image" />
->>>>>>> 59a74e2a5a66c7176f65c48a1ffa206f6f158c2c
-                                )}
+                                <Image src={item.image} className="news-image" />
+                            )}
                         </Item.Image>
                         <Item.Content>
                             {loading ? (
@@ -53,15 +66,15 @@ export default function NewsList() {
                                     </Placeholder.Paragraph>
                                 </Placeholder>
                             ) : (
-                                    <>
-                                        <Item.Header as='a' href={item.url}>{item.title}</Item.Header>
-                                        <Item.Description className="news-description">{item.description}</Item.Description>
-                                    </>
-                                )}
+                                <>
+                                    <Item.Header as="a" href={item.url}>{item.title}</Item.Header>
+                                    <Item.Description className="news-description">{item.description}</Item.Description>
+                                </>
+                            )}
                         </Item.Content>
                     </Item>
                 ))}
             </Item.Group>
         </div>
-    )
+    );
 }
