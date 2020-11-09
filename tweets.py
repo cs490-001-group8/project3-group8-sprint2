1aa10ae6dfa175794766360b948280564428dbac
Joseph Fiore <ec2-user@ip-172-31-2-238.ec2.internal>
2020-11-09 21:39:07 +0000

    Delete .DS_Store and fix style on tweets.py

--- tweets.py
+++ tweets.py
@@ -1,3 +1,7 @@
+"""
+    tweetpy
+    This file searches twitter for tweets
+"""
 import os
 from dotenv import load_dotenv
 import tweepy
@@ -5,8 +9,8 @@ import tweepy
 load_dotenv()
 
 #Set up authentication for the tweepy API using environmental variables set up like in the README
-auth = tweepy.OAuthHandler(os.getenv("TWITTER_CONSUMER_KEY"), os.getenv("TWITTER_CONSUMER_SECRET"))
-auth.set_access_token(os.getenv("TWITTER_ACCESS_TOKEN"), os.getenv("TWITTER_ACCESS_TOKEN_SECRET"))
+AUTH = tweepy.OAuthHandler(os.getenv("TWITTER_CONSUMER_KEY"), os.getenv("TWITTER_CONSUMER_SECRET"))
+AUTH.set_access_token(os.getenv("TWITTER_ACCESS_TOKEN"), os.getenv("TWITTER_ACCESS_TOKEN_SECRET"))
 
 #Get the tweepy object you can use to access the Twitter API
-api = tweepy.API(auth, wait_on_rate_limit=True)
+API = tweepy.API(AUTH, wait_on_rate_limit=True)
