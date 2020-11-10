460d8c261bb7376a7aa2345f2193ce25fb614bf1
Joseph Fiore <ec2-user@ip-172-31-2-238.ec2.internal>
2020-11-10 03:25:37 +0000

    Lint tweets_unit_tests.py

--- tests/tweets_unit_tests.py
+++ tests/tweets_unit_tests.py
@@ -1,18 +1,38 @@
+"""
+    tweets_unit_tests.py
+    This file does all tests for tweets.py
+"""
 import sys
 import unittest
 import unittest.mock as mock
-import datetime
 from os.path import dirname, join
+
+# pylint: disable=C0415
 sys.path.append(join(dirname(__file__), "../"))
 
+
 class TweetTestCases(unittest.TestCase):
+    """Make all the test cases"""
+
+    # pylint: disable=R0201
+    # pylint: disable=R0916
     def test_tweets_get_tweets(self):
+        """Test get_tweets"""
         mocker = mock.MagicMock()
         with mock.patch("tweepy.API", mocker):
             import tweets
+
             result = tweets.get_tweet("AAAA")
-            if "text" not in result or "sname" not in result or "ppic" not in result or "uname" not in result or "time" not in result or "date" not in result:
+            if (
+                    "text" not in result
+                    or "sname" not in result
+                    or "ppic" not in result
+                    or "uname" not in result
+                    or "time" not in result
+                    or "date" not in result
+            ):
                 raise ValueError
+
 
 if __name__ == "__main__":
     unittest.main()
