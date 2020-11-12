9ab23795ee073a39104d46c97f0c6a35e27d5544
Joseph Fiore <ec2-user@ip-172-31-2-238.ec2.internal>
2020-11-12 20:19:56 +0000

    pylint news_unit_tests

--- tests/tweets_unit_tests.py
+++ tests/tweets_unit_tests.py
@@ -1,116 +1,63 @@
 """
-    news_unit_tests.py
-    This file does all tests for news.py
+    tweets_unit_tests.py
+    This file does all tests for tweets.py
 """
 import sys
 import unittest
 import unittest.mock as mock
-import datetime
-import json
 from os.path import dirname, join
 
 # pylint: disable=C0415
 sys.path.append(join(dirname(__file__), "../"))
-from news import get_cache_news, get_latest_news
 
-KEY_INPUT = "input"
-KEY_METHOD = "method"
-KEY_EXPECTED = "expected"
 
-class MockedAPIResponse:
-
-    def __init__(self, text):
-        self.status_code = 200
-        self.text = text
-
-    def json(self):
-        return json.loads(self.text)
-
-class NewsTestCases(unittest.TestCase):
+class TweetTestCases(unittest.TestCase):
     """Make all the test cases"""
 
     # pylint: disable=R0201
     # pylint: disable=R0916
-    def setUp(self):
-        self.test_fetch_cache_success = {
-            KEY_INPUT: {
-                "timestamp": datetime.datetime.now().timestamp(),
-                "articles": [
-                    {
-                        "title": "Former \u2018Jersey Shore\u2019 Star Sammi Giancola Shows Off Long Legs In Short White Dress",
-                        "description": "Former Jersey Shore star Sammi \u201cSweetheart\u201d Giancola showed off her long legs in a short white dress in a new Instagram share. The former reality show star, who was seen in the original ...",
-                        "content": "Former Jersey Shore star Sammi \u201cSweetheart\u201d Giancola showed off her long legs in a short white dress in a new Instagram share. The former reality show star, who was seen in the original six seasons of the series, stunned her followers in the snap. He... [2288 chars]",
-                        "url": "https://www.inquisitr.com/6379236/jersey-shore-sammi-giancola-long-legs/",
-                        "image": "https://cdn.inquisitr.com/wp-content/uploads/2020/11/sammi-giancola.jpg",
-                        "publishedAt": "2020-11-11T17:13:59Z",
-                        "source": {
-                            "name": "The Inquisitr",
-                            "url": "https://www.inquisitr.com/"
-                        }
-                    }
-                ]
-            },
-            KEY_EXPECTED: [
-                {
-                    "title": "Former \u2018Jersey Shore\u2019 Star Sammi Giancola Shows Off Long Legs In Short White Dress",
-                    "description": "Former Jersey Shore star Sammi \u201cSweetheart\u201d Giancola showed off her long legs in a short white dress in a new Instagram share. The former reality show star, who was seen in the original ...",
-                    "content": "Former Jersey Shore star Sammi \u201cSweetheart\u201d Giancola showed off her long legs in a short white dress in a new Instagram share. The former reality show star, who was seen in the original six seasons of the series, stunned her followers in the snap. He... [2288 chars]",
-                    "url": "https://www.inquisitr.com/6379236/jersey-shore-sammi-giancola-long-legs/",
-                    "image": "https://cdn.inquisitr.com/wp-content/uploads/2020/11/sammi-giancola.jpg",
-                    "publishedAt": "2020-11-11T17:13:59Z",
-                    "source": {
-                        "name": "The Inquisitr",
-                        "url": "https://www.inquisitr.com/"
-                    }
-                }
-            ]
-        }
+    def test_tweets_get_tweets(self):
+        """Test get_tweets"""
+        mocker = mock.MagicMock()
+        with mock.patch(
+                "tweepy.API", mocker
+            ), mock.patch(
+                "tweepy.OAuthHandler", mocker
+            ):
+            import tweets
 
-        self.test_fetch_recent_success = {
-            KEY_INPUT: 
-                """{"articles": [
-                    {"title": "A","description": "B","content": "C","url": "D","image": "E","publishedAt": "F","source": {"name": "G","url": "H"}},
-                    {"title": "A","description": "B","content": "C","url": "D","image": "E","publishedAt": "F","source": {"name": "G","url": "H"}},
-                    {"title": "A","description": "B","content": "C","url": "D","image": "E","publishedAt": "F","source": {"name": "G","url": "H"}},
-                    {"title": "A","description": "B","content": "C","url": "D","image": "E","publishedAt": "F","source": {"name": "G","url": "H"}},
-                    {"title": "A","description": "B","content": "C","url": "D","image": "E","publishedAt": "F","source": {"name": "G","url": "H"}},
-                    {"title": "A","description": "B","content": "C","url": "D","image": "E","publishedAt": "F","source": {"name": "G","url": "H"}}
-                ]}""",
-            KEY_EXPECTED: [
-                {"title": "A","description": "B","content": "C","url": "D","image": "E","publishedAt": "F","source": {"name": "G","url": "H"}},
-                {"title": "A","description": "B","content": "C","url": "D","image": "E","publishedAt": "F","source": {"name": "G","url": "H"}},
-                {"title": "A","description": "B","content": "C","url": "D","image": "E","publishedAt": "F","source": {"name": "G","url": "H"}},
-                {"title": "A","description": "B","content": "C","url": "D","image": "E","publishedAt": "F","source": {"name": "G","url": "H"}},
-                {"title": "A","description": "B","content": "C","url": "D","image": "E","publishedAt": "F","source": {"name": "G","url": "H"}}
-            ]
-        }
+            result = tweets.get_tweet("AAAA")
+            if (
+                    "text" not in result
+                    or "sname" not in result
+                    or "ppic" not in result
+                    or "uname" not in result
+                    or "time" not in result
+                    or "date" not in result
+            ):
+                raise ValueError
 
     # pylint: disable=R0201
     # pylint: disable=R0916
-    def test_news_get_cache(self):
-        p1 = mock.patch( "builtins.open", mock.MagicMock() )
-        m = mock.MagicMock( side_effect = [ self.test_fetch_cache_success[KEY_INPUT] ] )
-        p2 = mock.patch( "json.load", m )
+    def test_tweets_get_politicians(self):
+        """Test get_politicians_latest_tweets"""
+        mocker = mock.MagicMock()
+        with mock.patch(
+                "tweepy.API", mocker
+            ), mock.patch(
+                "tweepy.OAuthHandler", mocker
+            ):
+            import tweets
 
-        with p1, p2:
-            result = get_cache_news()
+            result = tweets.get_politicians_latest_tweets()
+            if (
+                    "gov" not in result
+                    or "sen" not in result
+                    or not isinstance(result["sen"], list)
+                    or len(result["sen"]) != 2
+            ):
+                raise ValueError
 
-        self.assertEqual(result, self.test_fetch_cache_success[KEY_EXPECTED])
-        
-    # pylint: disable=R0201
-    # pylint: disable=R0916
-    def test_news_get_recent(self):
-        p1 = mock.patch( "requests.get", self.mocked_requests )
-        m2 = mock.MagicMock( side_effect = [ None ] )
-        p2 = mock.patch( "news.get_cache_news", m2 )
-
-        with p1, p2:
-            result = get_latest_news()
-
-        self.assertEqual(result, self.test_fetch_recent_success[KEY_EXPECTED])
-        
-    def mocked_requests(self, url, params):
-        return MockedAPIResponse(self.test_fetch_recent_success[KEY_INPUT])
 
 if __name__ == "__main__":
     unittest.main()
