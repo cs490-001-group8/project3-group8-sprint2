53c611348ad55f92ce6db093f621fba700301a64
Joseph Fiore <ec2-user@ip-172-31-2-238.ec2.internal>
2020-11-16 23:34:52 +0000

    Style image failed to load text

--- scripts/Politicians_React/PoliticianPhoto.jsx
+++ scripts/Politicians_React/PoliticianPhoto.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export default function PoliticianPhoto({ photo }) {
     return (
-        <img className="politician-photo" src={photo} alt="" />
+        <img className="politician-photo" src={photo} alt="This person has not uploaded their photo" />
     );
 }
 
