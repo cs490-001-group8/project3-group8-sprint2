9184f7a0c2a31cad8f0de0213c351cf332668514
Ameer Hassan <ec2-user@ip-172-31-64-222.ec2.internal>
2020-11-26 21:35:32 +0000

    proper icon fields and layouts

--- scripts/TabChooser.jsx
+++ scripts/TabChooser.jsx
@@ -2,16 +2,58 @@
 // Proptype enforcement breaks the module.
 import React from 'react';
 
+const unselected = "unselected"
+const selected = "selected"
+
+function handleClick(data){
+    console.log(data)
+}
+
 export default function TabChooser(props) {
     return (
         <div className="icon-bar">
-            <i title="News" className="far fa-newspaper fa-2x"></i>
-            <i title="Weather" className="fas fa-sun fa-2x"></i>
-            <i title="Traffic" className="fas fa-traffic-light fa-2x"></i>
-            <i title="Bills" className="fas fa-vote-yea fa-2x"></i>
-            <i title="Politican Twitter" className="fab fa-twitter fa-2x"></i>
-            <i title="Hiking Destinations" className="fas fa-mountain fa-2x"></i>
-            <i title="Sports" className="fas fa-football-ball fa-2x"></i>
+            <i
+                id="News"
+                title="News" 
+                className="far fa-newspaper fa-2x"
+                onClick={() => handleClick('News')}>
+            </i>
+            <i 
+                id="Weather" 
+                title="Weather" 
+                className="fas fa-sun fa-2x"
+                onClick={() => handleClick('Weather')}>
+            </i>
+            <i
+                id="Traffic"
+                title="Traffic" 
+                className="fas fa-traffic-light fa-2x"
+                onClick={() => handleClick('Traffic')}>
+            </i>
+            <i
+                id="Bills"
+                title="Bills" 
+                className="fas fa-vote-yea fa-2x"
+                onClick={() => handleClick('Bills')}>
+            </i>
+            <i
+                id="Politician_Twitter"
+                title="Politican Twitter" 
+                className="fab fa-twitter fa-2x"
+                onClick={() => handleClick('Politician_Twitter')}>
+            </i>
+            <i
+                id="Hiking_Destinations"
+                title="Hiking Destinations" 
+                className="fas fa-mountain fa-2x"
+                onClick={() => handleClick('Hiking_Destinations')}>
+            </i>
+            <i
+                id="Sports"
+                title="Sports" 
+                className="fas fa-football-ball fa-2x"
+                onClick={() => handleClick('Sports')}>
+            </i>
         </div>
     );
 }
